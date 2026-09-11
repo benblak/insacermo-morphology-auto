68259f032171bc29e050f7407545671a43746082
benblak <Benblakus@gmail.com>
2026-09-11 15:59:54 +0200

    Import Bellman PRESERVE module

--- formal/actionability_information/InsacermoActionabilityInformation.lean
+++ formal/actionability_information/InsacermoActionabilityInformation.lean
@@ -2,3 +2,4 @@ import InsacermoActionabilityInformation.Decomposition
 import InsacermoActionabilityInformation.BiMonotone
 import InsacermoActionabilityInformation.SequentialPreserve
 import InsacermoActionabilityInformation.SequentialPlanner
+import InsacermoActionabilityInformation.BellmanPreserve
