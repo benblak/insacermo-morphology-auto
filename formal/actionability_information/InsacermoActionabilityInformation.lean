97c644d2a217210af76a5444cd3daa1b338edba4
benblak <Benblakus@gmail.com>
2026-09-20 23:11:44 +0200

    import SCC structural audit bridge

--- formal/actionability_information/InsacermoActionabilityInformation.lean
+++ formal/actionability_information/InsacermoActionabilityInformation.lean
@@ -44,3 +44,5 @@ import InsacermoActionabilityInformation.RepairPlanDuality
 import InsacermoActionabilityInformation.MasterClosureV1
 
 import InsacermoActionabilityInformation.StructuralAuditKernel
+
+import InsacermoActionabilityInformation.StructuralAuditSCCBridge
